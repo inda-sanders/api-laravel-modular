cb8259b42e7a11311f62b423cbbcdb81fb5d6304
Satrio Rully Priyambodo <80313600+Fslink32@users.noreply.github.com>
2025-02-21 14:42:46 +0700

    update token validation

--- app/Http/Middleware/EnsureApiTokenIsValid.php
+++ app/Http/Middleware/EnsureApiTokenIsValid.php
@@ -22,21 +22,23 @@ class EnsureApiTokenIsValid extends Controller
     {
         $apiKey = $request->header('x-api-key') ?? null;
 
-        $client = Client::where('api_key', $apiKey)->first();
-
-        if (!$client) {
-            return response()->json(['responseCode' => 401, 'message' => 'Invalid api key', 'data' => []], 200);
-        }
-
         if ($allowedApiKeys && !$apiKey) {
             return response()->json(['responseCode' => 400, 'message' => 'Missing x-api-key', 'data' => []], 200);
+        }else if ($allowedApiKeys && $apiKey) {
+            $client = Client::where('api_key', $apiKey)->first();
+
+            if (!$client) {
+                return response()->json(['responseCode' => 401, 'message' => 'Invalid api key', 'data' => []], 200);
+            }
         }
+
+
 
         if (!empty($allowedApiKeys) && !in_array($client->name, $allowedApiKeys)) {
             return response()->json(['responseCode' => 403, 'message' => 'Unauthorized api key', 'data' => []], 200);
         }
 
-        $request->merge(['client' => $client->name,'client_id'=>$client->id]);
+        $request->merge(['client' => $client->name, 'client_id' => $client->id]);
 
         if ($request->hasHeader('Authorization')) {
             $token = $request->bearerToken();
